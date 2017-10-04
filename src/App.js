4894fb8a66ed970e6cbc2b98fa92da9db8e07a8e
kc657 <kvn.0218@gmail.com>
2017-10-04 15:31:39 -0700

    added login form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, {Component} from 'react'
 import {View, Text} from 'react-native'
 import {Header} from './components/common'
+import firebase from 'firebase'
 
 class App extends Component {
+  componentWillMount = () => {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDeKuhe0J3CUUdB5S49Q1x5nwhiaMiBVOQ',
+      authDomain: 'auth-reactnative-b0d11.firebaseapp.com',
+      databaseURL: 'https://auth-reactnative-b0d11.firebaseio.com',
+      projectId: 'auth-reactnative-b0d11',
+      storageBucket: 'auth-reactnative-b0d11.appspot.com',
+      messagingSenderId: '186955592850'
+    })
+  }
+  
   render () {
     return (
       <View>
